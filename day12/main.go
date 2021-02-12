a5fa591b791e48f18f4d4fd746962ead3b5f3d2f
KS_HTK <HKoertge@gmx.de>
2021-02-12 20:08:42 +0100

    added day12 Part 2

--- day12/main.go
+++ day12/main.go
@@ -44,44 +44,45 @@ var sqclose = []byte("]")[0]
 var red = []byte("red")
 
 func sumred(start int, end int, dat []byte) int {
-	if start+3 > end {
-		return 0
-	}
 	first := -1
-	last := len(dat)
-	//search first opening bracket
 	for i := start; i < end; i++ {
 		if dat[i] == open {
 			first = i
 			break
 		}
 	}
-	//if non found return 0
+	//if non found or start > end return 0
 	if first == -1 {
 		return 0
 	}
-	//search the matching closing bracket
-	for j := first + 1; j < end; j++ {
+
+	//search the matching closing bracket and look if red is contained
+	last := -1
+	containsRed := false
+	for j := first; j < end; j++ {
 		c := dat[j]
 		count := 0
 		if c == open || c == sqopen {
 			count++
 		} else if c == close || c == sqclose {
+			//count can only be 0 if there is a "}" bracket.
 			if count == 0 {
 				last = j
 				break
 			} else {
 				count--
 			}
-		} else if count == 0 {
-			if c == red[0] && dat[j+1] == red[1] && dat[j+2] == red[2] {
-				//found word red, return -sum of all numbers between first and last
-				part := dat[first+1 : last]
-				nums := getnums(string(part))
-				//return negativ sum + potential same level exclusions
-				return sumred(last+1, end, dat) + sum(nums)
-			}
+		} else if !containsRed && count == 0 && c == red[0] && dat[j+1] == red[1] && dat[j+2] == red[2] {
+			containsRed = true
+			fmt.Println("Code RED!")
 		}
+	}
+	if containsRed {
+		//found word red, return -sum of all numbers between first and last
+		part := dat[first+1 : last]
+		nums := getnums(string(part))
+		//return sum + potential same level exclusions
+		return sumred(last+1, end, dat) + sum(nums)
 	}
 	//if no red on this level
 	return sumred(first+1, last, dat) + sumred(last+1, end, dat)
