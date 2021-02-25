a92376d64481ef41ec6a044dcf55e769d091f45e
KS_HTK <HKoertge@gmx.de>
2021-02-26 00:12:47 +0100

    solved day13

---
+++ day13/go.mod
@@ -0,0 +1,5 @@
+module github.com/ks_htk/adventOfCode15/day13
+
+go 1.16
+
+require modernc.org/mathutil v1.2.2
--- day13/main.go
+++ day13/main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"sort"
+	"strconv"
 	"strings"
+
+	"modernc.org/mathutil"
 )
 
 func errchk(e error) {
@@ -12,31 +16,79 @@ func errchk(e error) {
 	}
 }
 
+var dataMap map[string](map[string]int)
+
+func interpret(lines []string) {
+	dataMap = make(map[string]map[string]int)
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+		words := strings.Split(line, " ")
+		if dataMap[words[0]] == nil {
+			dataMap[words[0]] = make(map[string]int)
+		}
+		val, err := strconv.Atoi(words[3])
+		errchk(err)
+		if words[2] == "lose" {
+			val = -val
+		}
+		dataMap[words[0]][words[len(words)-1]] = val
+	}
+}
+
+func keys(in map[string]map[string]int) []string {
+	out := make([]string, 0, len(in))
+	for k := range in {
+		out = append(out, k)
+	}
+	return out
+}
+
+//quick and dirty fix for % being a shitty remainder and not a mod function
+//!only for positiv b!
+func mod(a int, b int) int {
+	out := a % b
+	if out < 0 {
+		return out + b
+	}
+	return out
+}
+
+func calcHappieness(names []string) int {
+	score := 0
+	for i, name := range names {
+		score += dataMap[name][names[mod(i-1, len(names))]]
+		score += dataMap[name][names[mod(i+1, len(names))]]
+	}
+	return score
+}
+
+func findMaxHappieness() int {
+	keySet := sort.StringSlice(keys(dataMap))
+	mathutil.PermutationFirst(keySet)
+	max, score := 0, 0
+	for mathutil.PermutationNext(keySet) {
+		score = calcHappieness(keySet)
+		if score > max {
+			max = score
+		}
+	}
+	return max
+}
+
 func main() {
 	dat, err := ioutil.ReadFile("input")
 	errchk(err)
 	lines := strings.Split(string(dat), ".\n")
+	interpret(lines)
 
-	fmt.Println(lines)
-	fmt.Printf("Part 1: %d\n", pt1(string(dat)))
-	fmt.Printf("Part 2: %d\n", pt2(string(dat)))
-}
-
-func pt1(input string) int {
-	return strings.Count(input, "(") - strings.Count(input, ")")
-}
-
-func pt2(input string) int {
-	lv := 0
-	for i, c := range input {
-		if lv == -1 {
-			return i
-		}
-		if string(c) == "(" {
-			lv++
-		} else {
-			lv--
-		}
+	fmt.Printf("Part 1: %d\n", findMaxHappieness())
+	keySet := keys(dataMap)
+	dataMap["me"] = make(map[string]int)
+	for _, name := range keySet {
+		dataMap[name]["me"] = 0
+		dataMap["me"][name] = 0
 	}
-	return -1
+	fmt.Printf("Part 2: %d\n", findMaxHappieness())
 }
