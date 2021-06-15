72cb31b6b3341d191e5141e1aab0b8726977eb7f
KS_HTK <HKoertge@gmx.de>
2021-06-15 19:02:38 +0200

    modified day19 reversed solving attempt to go from medicin to 'e' runs for small testinput locks up for actuall input

--- day19/main.go
+++ day19/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"sort"
 	"strings"
 	"sync"
 )
@@ -37,7 +38,7 @@ func interpret(lines []string, startStr chan<- string) map[string][]string {
 }
 
 //count the amount of transformations possibel on the in string useing the t(ransition) map
-func getResultCount(in string, t map[string][]string) int {
+func countTransformations(in string, t map[string][]string) int {
 	return len(getAllTransformations(in, t))
 }
 
@@ -68,66 +69,124 @@ func getAllTransformations(in string, t map[string][]string) map[string]int {
 	return resultMap
 }
 
+//Helper Function: get index of all occurences of substring in string
+func indexAll(str string, substr string) []int {
+	ind := make(map[int]int, 0)
+	center := len(str) / 2
+	if center < len(substr) {
+		for i := 0; i < len(str); i++ {
+			index := strings.Index(str[i:], substr)
+			if index != -1 {
+				ind[index+i] = 0
+			}
+		}
+	} else {
+		for i := 0; i <= center; i++ {
+			index := strings.Index(str[i:center], substr)
+			lIndex := strings.LastIndex(str[center:len(str)-i], substr)
+			if index != -1 {
+				ind[index+i] = 0
+			}
+			if lIndex != -1 {
+				ind[lIndex+center] = 0
+			}
+		}
+	}
+	out := make([]int, 0)
+	for k := range ind {
+		out = append(out, k)
+	}
+	sort.Ints(out)
+	return out
+}
+
+//Helper Function: reverse the Mappings and sort them by Length
+func reverseTransitions(in map[string][]string) map[string]string {
+	out := make(map[string]string)
+	for key, val := range in {
+		for _, v := range val {
+			if exists, ok := out[v]; ok {
+				fmt.Println("Value Overwritten: ", v, exists, key)
+			}
+			out[v] = key
+		}
+	}
+	return out
+}
+
 //job strct stores a molecule and the amount of steps used to synthesize it
 type job struct {
 	mol  string
+	prev string
 	step int
 }
 
 //store all molecules and minimal steps to get to them in the molindex
 //if a mol has already been indexed do not add it to the todo channel
 //if the med molecule has been reached close all further jobs
-func indexer(jobs <-chan job, todo chan<- job, med string) int {
+func indexer(jobs <-chan job, todo chan<- job, med string, res chan<- int) {
 	//store all found molecules and the min steps to synthesize it
-	molindex := make(map[string]int)
+	molindex := make(map[string]job)
 	open := true
 	for j := range jobs {
-		if j.mol == med {
+		if j.mol == "e" {
 			if open {
 				open = false
 				close(todo)
 			}
-		}
-		if len(j.mol) > len(med) {
+		} else if strings.Contains(j.mol, "e") {
+			//if string contains any "e" discard any further job
 			continue
 		}
-		if min, ok := molindex[j.mol]; ok {
-			if min > j.step {
-				molindex[j.mol] = j.step
+		if mol, ok := molindex[j.mol]; ok {
+			if mol.step > j.step {
+				molindex[j.mol] = j
 			}
 		} else {
-			molindex[j.mol] = j.step
+			molindex[j.mol] = j
 			if open {
 				todo <- j
 			}
 		}
 	}
-	return molindex[med]
+	res <- getStepCount(molindex)
 }
 
 //worker for generating the future jobs based on a incomming job
-func worker(jobs <-chan job, addJob chan<- job, t map[string][]string) {
+func worker(jobs <-chan job, addJob chan<- job, rt map[string]string) {
 	for j := range jobs {
-		lst := getAllTransformations(j.mol, t)
-		for key := range lst {
-			addJob <- job{mol: key, step: j.step + 1}
+		for key, val := range rt {
+			ind := indexAll(j.mol, key)
+			for _, i := range ind {
+				molecule := j.mol[:i] + val + j.mol[i+len(key):]
+				addJob <- job{mol: molecule, prev: j.mol, step: j.step + 1}
+			}
 		}
 	}
 }
 
+//function for tracing the steps trough the map
+func getStepCount(m map[string]job) int {
+	count := 0
+	j := m["e"]
+	for j.prev != "" {
+		j = m[j.prev]
+		count++
+	}
+	return count
+}
+
 //function for initalizing indexer and worker and overseeing their work
-func makeMed(med string, t map[string][]string, res chan int) {
+func makeMed(med string, rt map[string]string, res chan<- int) {
 	jobsForIndexer := make(chan job, 10000000)
 	jobsForWorkers := make(chan job, 10000000)
-	go func() {
-		res <- indexer(jobsForIndexer, jobsForWorkers, med)
-	}()
-	jobsForIndexer <- job{mol: "e", step: 0}
+	go indexer(jobsForIndexer, jobsForWorkers, med, res)
+	jobsForIndexer <- job{mol: med, step: 0, prev: ""}
 	var wg sync.WaitGroup
 	for i := 0; i < 12; i++ {
 		wg.Add(1)
 		go func() {
-			worker(jobsForWorkers, jobsForIndexer, t)
+			worker(jobsForWorkers, jobsForIndexer, rt)
 			wg.Done()
 		}()
 	}
@@ -147,10 +206,11 @@ func main() {
 		transitions = interpret(lines, strChannel)
 	}()
 	med = <-strChannel
+
 	res1, res2 := make(chan int), make(chan int)
 	go func() {
-		res1 <- getResultCount(med, transitions)
-		makeMed(med, transitions, res2)
+		res1 <- countTransformations(med, transitions)
+		makeMed(med, reverseTransitions(transitions), res2)
 	}()
 	fmt.Printf("Part 1: %d\n", <-res1)
 	fmt.Printf("Part 2: %d\n", <-res2)
